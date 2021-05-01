35937c0a9cd7804d9ee1c0496f77fb6bd0bf0aac
bigcatplichta <bigcatplichta@gmail.com>
2021-04-30 23:03:47 -0600

    update content

---
+++ content/posts/i-made-this-for-you.md
@@ -0,0 +1,8 @@
+---
+title: I made this for you
+date: '2021-04-29T03:46:03.835Z'
+type: post
+path: /blog/i-made-this-for-you
+draft: true
+---
+## I made this for you
--- src/components/siteLayout.js
+++ src/components/siteLayout.js
@@ -39,6 +39,24 @@ const MasterLayout = ({ children }) => {
   )
 }
 
+// Copied from the docs, doesn't play well with current site setup...
+// const CreatePostPlugin = new RemarkCreatorPlugin({
+//   label: 'New Blog Post',
+//   filename: form => {
+//     return form.filename
+//   },
+//   fields: [
+//     {
+//       name: 'filename',
+//       component: 'text',
+//       label: 'Filename',
+//       placeholder: 'content/blog/hello-world/index.md',
+//       description:
+//         'The full path to the new Markdown file, relative to the repository root.',
+//     },
+//   ],
+// })
+
 // This copied from CreatePostButton, need to write with updated method
 // Should probably upgrade TinaCMS first which doesn't play nice with other dependencies somehow...
 const CreateAnimalButton = createRemarkButton({
